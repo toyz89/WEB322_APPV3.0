704f6e57efafd7db20c6ec56a866ca9533200795
Xiaochen Wang <ryan.wang23@hotmail.com>
2017-05-21 20:31:18 -0400

    add declaration for submission

---
+++ server.js
@@ -0,0 +1,35 @@
+/*********************************************************************************
+*  WEB322 –Assignment02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: ______________________ Student ID: ______________ Date: ________________
+*
+*  Online (Heroku) Link: ________________________________________________________
+*
+********************************************************************************/ 
+var express = require("express");
+var app = express();
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+   res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
+});
+
+// setup another route to listen on /about
+app.get("/about", function(req,res){
+   res.send("<h3>About</h3>");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
+
+app.use(express.static('public'));
